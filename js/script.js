b94a806f0e988b2542824efc3daa5e4303113d56
murkit <amgid7@mail.ru>
2022-01-14 01:04:03 +0300

    Задание с корзиной Базовый JavaScript

--- js/script.js
+++ js/script.js
@@ -4,3 +4,38 @@ let headerMenuBtn = document.querySelector('.header__menu-btn'),
 headerMenuBtn.addEventListener('click', function() {
     headerNav.style.display == 'none'? headerNav.style.display = 'block': headerNav.style.display = 'none'; 
 });
+
+
+let product = document.querySelectorAll('.products__item'),
+    basket = document.querySelector('.basket'),
+    addProduct = document.querySelector('.add__product'),
+    table = document.querySelector('.table-insert'),
+    count = 1,
+    price = 0;
+
+product.forEach(btn => {
+    btn.addEventListener('click', headlerClick);
+});
+
+ function headlerClick(event) {
+    addProducts(event);
+    console.log('1');
+
+}
+
+function addProducts (event){
+    count++;
+    addProduct.textContent = `${count}`;
+    price = event.path[2].querySelector('.products__item-price').textContent.slice(1);
+    let product_name = event.path[2].querySelector('.products__item-title').textContent;
+    table.insertAdjacentHTML('afterend',
+    `   <td class="table-item item__name">${product_name}</td>></td>
+        <td class="table-item item__count">${count}</td>
+        <td class="table-item item__price">${price}</td>
+        <td class="table-item item__total">${price*count}</td>
+    `
+    
+    )
+
+}
+
