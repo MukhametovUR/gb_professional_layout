96738d870e241efd406ed21685ff1f2be8f835f4
MukhametovUR <ural-lan4x4@yandex.ru>
2022-01-17 17:38:30 +0300

    Сверстана страница catalog.html, реализована адаптивная верстка, добавлена навигация по фильтации товаров

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 'use strict';
 let headerMenuBtn = document.querySelector('.header__menu-btn'),
-    headerNav = document.querySelector('.header__nav');
+    headerNav = document.querySelector('.header__nav'),
+
+    filterBtn = document.querySelector('.filter__main'),
+    // filterSecBtn = document.querySelector('.filter__main-second'),
+    mainFilter = document.querySelector('.filter__nav');
+
 headerMenuBtn.addEventListener('click', function() {
     headerNav.style.display == 'none'? headerNav.style.display = 'block': headerNav.style.display = 'none'; 
 });
+
+filterBtn.addEventListener('click', function() {
+    mainFilter.style.display == 'none'? mainFilter.style.display = 'block': mainFilter.style.display = 'none'; 
+    // filterBtn.style.display == 'block'? mainFilter.style.display = 'none': mainFilter.style.display = 'block'; 
+    console.log('click');
+});
+
+
